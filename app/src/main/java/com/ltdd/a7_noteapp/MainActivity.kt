4dbc4469029771703703fe1604f112446689c81d
NguyenDucThanhNhan <ngducthanhnhan@gmail.com>
2024-03-29 16:50:35 +0700

    Add random color to item

--- app/src/main/java/com/ltdd/a7_noteapp/MainActivity.kt
+++ app/src/main/java/com/ltdd/a7_noteapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
@@ -19,6 +20,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.collection.LLRBNode.Color
 import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
@@ -26,6 +28,8 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ltdd.a7_noteapp.databinding.ActivityMainBinding
 import com.ltdd.a7_noteapp.model.Post
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         auth = Firebase.auth
 
         database = Firebase.database
-        myRef = database.getReference("message")
+        myRef = database.getReference("posts")
         firestore = Firebase.firestore
 
         binding.rvNotes.layoutManager = GridLayoutManager(
@@ -53,7 +57,9 @@ class MainActivity : AppCompatActivity() {
             false
         )
 
-        addNotes()
+        binding.btnAdd.setOnClickListener {
+            addNotes()
+        }
     }
 
     fun addNotes(){
@@ -62,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         val content = "Test content"
 
         if (id != null) {
-            myRef.child(id).setValue(Post(id, title, content)).addOnCompleteListener { task ->
+            myRef.child(id).setValue(Post(id, title, content, gotRandomColor())).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Log.d("DEBUG", "post data successful")
                 } else {
@@ -91,6 +97,7 @@ class MainActivity : AppCompatActivity() {
             protected override fun onBindViewHolder(holder: PostHolder, position: Int, model: Post) {
                 holder.txtTitle.text = model.title
                 holder.txtContent.text = model.content
+                holder.layoutNote.setBackgroundColor(android.graphics.Color.parseColor(model.color))
             }
 
             override fun onDataChanged() {
@@ -104,13 +111,51 @@ class MainActivity : AppCompatActivity() {
     class PostHolder(view: View) : RecyclerView.ViewHolder(view) {
         var txtTitle: TextView
         var txtContent: TextView
+        var layoutNote: CardView
 
         init {
             txtTitle = view.findViewById(R.id.txt_title)
             txtContent = view.findViewById(R.id.txt_content)
+            layoutNote = view.findViewById(R.id.layoutNote)
         }
     }
 
+    fun gotRandomColor(): String {
+        val colors = ArrayList<String>()
+        colors.add("#FFD1DC")
+        colors.add("#FFA07A")
+        colors.add("#FFB6C1")
+        colors.add("#87CEEB")
+        colors.add("#98FB98")
+        colors.add("#FF69B4")
+        colors.add("#ADD8E6")
+        colors.add("#F08080")
+        colors.add("#90EE90")
+        colors.add("#FF6347")
+        colors.add("#AFEEEE")
+        colors.add("#DB7093")
+        colors.add("#FFE4E1")
+        colors.add("#FFE4B5")
+        colors.add("#F0FFF0")
+        colors.add("#BA55D3")
+        colors.add("#B0C4DE")
+        colors.add("#7B68EE")
+        colors.add("#20B2AA")
+        colors.add("#9370DB")
+        colors.add("#3CB371")
+        colors.add("#FF7F50")
+        colors.add("#4682B4")
+        colors.add("#556B2F")
+        colors.add("#D8BFD8")
+        colors.add("#5F9EA0")
+        colors.add("#DAA520")
+        colors.add("#B8860B")
+        colors.add("#2E8B57")
+        colors.add("#008080")
+
+        val random = Random()
+        return colors[random.nextInt(colors.size)]
+    }
 
     fun login(email: String, password: String){
         auth.signInWithEmailAndPassword(email, password)
--- app/src/main/java/com/ltdd/a7_noteapp/model/Post.kt
+++ app/src/main/java/com/ltdd/a7_noteapp/model/Post.kt
@@ -1,5 +1,5 @@
 package com.ltdd.a7_noteapp.model
 
-class Post(var id: String = "", var title: String = "", var content: String = "") {
-    constructor() : this("", "", "") // constructor không đối số
+class Post(var id: String = "", var title: String = "", var content: String = "", var color: String = "") {
+    constructor() : this("", "", "", "") // constructor không đối số
 }
